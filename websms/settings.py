4ffd4c4ca65e434daa573f6b9dd3d3750591ae84
ph00lt0 <->
2022-02-05 11:51:37 +0000

    add otp

--- websms/settings.py
+++ websms/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_otp',
+    'django_otp.plugins.otp_totp',
+
+    'otp_twilio',
+    'otp_yubikey',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_otp.middleware.OTPMiddleware'
 ]
 
 ROOT_URLCONF = 'websms.urls'

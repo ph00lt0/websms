06d2ff369b6facb4a8eaa88c5efd532044bd9271
ph00lt0 <->
2022-02-05 15:16:00 +0000

    allow host

--- websms/settings.py
+++ websms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@mt_-w1^)!rmborsnu^n70z2lgezse&h6nr3hvert2y)&u3g$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hackaway.software']
 
 
 # Application definition
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 
     'otp_twilio',
     'otp_yubikey',
+    'qrcode',
+
+    'account'
 ]
 
 MIDDLEWARE = [

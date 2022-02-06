2b0ee18e2fb3c9cbececf709d9a2ad32c53cccd6
ph00lt0 <->
2022-02-06 08:36:56 +0000

    static debug

--- websms/urls.py
+++ websms/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from django_otp.forms import OTPAuthenticationForm
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

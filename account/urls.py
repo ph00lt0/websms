6783b4b8c6ec61c40b123a36329a9bad1c3b3b0c
mkai121 <mkai121@live.rhul.ac.uk>
2022-02-06 11:22:39 +0000

    Change ''

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('sign_up/', views.sign_up, name='sign_up'),
